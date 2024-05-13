088a3f6f3b48abb7fd2c8fd15c4491e9a5ae0c1a
zhujiahong <vermilionand@gmail.com>
2024-05-13 14:41:40 +0800

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'leetcode 有openApi吗',
+  tagline: '鹏宝可见',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
